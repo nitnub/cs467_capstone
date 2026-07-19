eab89f43738ea5aa37483198137e1d1397d3f78b
fwLaptop <nick@nitnub.io>
2026-07-18 17:27:12 -0700

    add video module proof of concept printing memory array to screen

--- spike_documents_nb/video_poc/video_startup.h
+++ spike_documents_nb/video_poc/video_startup.h
@@ -1,5 +1,4 @@
 
-
 #ifndef VIDEO_STARTUP_H
 #define VIDEO_STARTUP_H
 #include <SDL2/SDL_rect.h>
@@ -7,66 +6,53 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-
+#define WINDOW_TITLE "Intel 8080 - Space Invaders" // TODO: temp placeholder name
 #define PIXEL_WIDTH 224
 #define PIXEL_HEIGHT 256
-#define SCREEN_WIDTH 940 // 800
-#define SCREEN_HEIGHT 1272 // 600
+#define SCREEN_WIDTH 940 // test scaling
+#define SCREEN_HEIGHT 1272 // test scaling
 
-#define HEX_SIZE 16
 #define BYTE_SIZE 8
-#define SET_BIT = 1
-#define CLEAR_BIT = 0
-// #define PIXEL_WIDTH 256
-// #define PIXEL_HEIGHT 224
-// #define SCREEN_WIDTH 672 //768 // 800
-// #define SCREEN_HEIGHT 768 //672 // 600
+#define SET_BIT '1'
+#define CLEAR_BIT '0'
 
-// typedef struct {
-//     SDL_Point white[681];
-//     SDL_Point *green[681];
-//     SDL_Point *red;
-// } Points_t;
-
-// struct Game {
-//     SDL_Window *window;
-//     SDL_Renderer *renderer;
-//     SDL_Texture *texture;
-//     // SDL_Point *points;
-//     Points_t *pointsArray;
-// };
-typedef struct {
-    int white;
-    int green;
-    int red;
-} Point_Counter_t;
 
 typedef struct {
-    SDL_Point white[681];
-    SDL_Point green[681];
-    SDL_Point red[681];
-
+    SDL_Point white[6800];  // TODO: test sizing.. calc specific values for each color
+    SDL_Point green[6800];  // TODO: test sizing.. calc specific values for each color
+    SDL_Point red[6800];    // TODO: test sizing.. calc specific values for each color
     int wCount;
     int gCount;
     int rCount;
 } Points_t;
 
-struct Game {
+typedef struct  {
     SDL_Window *window;
     SDL_Renderer *renderer;
     SDL_Texture *texture;
-    // SDL_Point *points;
-    Points_t pointsArray;
-};
+    Points_t points;
+} Game;
 
 
+/**
+ * Call to initialize the video module.
+ * @param game the current game's SDL data struct
+ * @return 0 if no error; non-zero for error
+ */
+int sdlVideoInit(Game *game);
 
+/**
+ * Call to safely shut down the video module.
+ * @param game the current game's SDL data struct
+ * @param exit_status status / error code to exit with
+ */
+void sdlVideoCleanup(Game *game, int exit_status);
 
-void printStartup();
+/**
+ * Update the three color group's points arrays and counts.
+ * @param points the Points_t struct containing point info for the renderer
+ * @return count of points created
+ */
+int updatePoints(Points_t *points);
 
-// int getPointsArray(SDL_Point **points);
-// SDL_Point *getPointsArray(int *pCount);
-
-int getPointsArray(Points_t *points);
-SDL_Point *getPointArrayTest();
 #endif //VIDEO_STARTUP_H
