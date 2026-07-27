1972433084afcccc46f5dc9c8e46cdf8aea75f20
fwLaptop <nick@nitnub.io>
2026-07-27 03:56:21 -0700

    add a basic shared program loop to main

---
+++ video/video.h
@@ -0,0 +1,75 @@
+
+#ifndef VIDEO_STARTUP_H
+#define VIDEO_STARTUP_H
+#include <SDL2/SDL_rect.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+// #include "../cpu/cpu.h"
+#include "../core/cpu.h"
+
+
+#define WINDOW_TITLE "Intel 8080 - Space Invaders" // TODO: temp placeholder name
+#define PIXEL_WIDTH 224
+#define PIXEL_HEIGHT 256
+#define SCREEN_WIDTH 940 // test scaling
+#define SCREEN_HEIGHT 1272 // test scaling
+
+#define BYTE_SIZE 8
+#define SET_BIT '1'
+#define CLEAR_BIT '0'
+
+
+typedef struct {
+    SDL_Point white[6800];  // TODO: test sizing.. calc specific values for each color
+    SDL_Point green[6800];  // TODO: test sizing.. calc specific values for each color
+    SDL_Point red[6800];    // TODO: test sizing.. calc specific values for each color
+    SDL_Point black[520192];    // TODO: test sizing.. calc specific values for each color
+    int wCount;
+    int gCount;
+    int rCount;
+    int bCount;
+} Points_t;
+
+typedef struct  {
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Texture *texture;
+    Points_t points;
+} Media_t;
+
+
+/**
+ * Initialize and return a media bucket struct to house A/V structs created to work with SDL2.
+ * @return an allocated media bucket struct
+ */
+Media_t *initMedia(void);
+
+// int sendVideoInterrupt(long *lastMs);
+int sendVideoInterrupt(state *s, long *lastMs);
+
+/**
+ * Call to initialize the video module.
+ * @param game the current game's SDL data struct
+ * @return 0 if no error; non-zero for error
+ */
+int sdlVideoInit(Media_t *game);
+
+/**
+ * Call to safely shut down the video module.
+ * @param game the current game's SDL data struct
+ * @param exit_status status / error code to exit with
+ */
+void sdlVideoCleanup(Media_t *game, int exit_status);
+
+/**
+ * Update the three color group's points arrays and counts.
+ * @param points the Points_t struct containing point info for the renderer
+ * @return count of points created
+ */
+int updatePoints(state *s, Points_t *points);
+
+
+int drawScreen(state *s, Media_t *game);
+
+#endif //VIDEO_STARTUP_H
---
+++ video/windowManager_temp.c
@@ -0,0 +1,50 @@
+// Temp logic to manage closing / advancing the window for testing
+
+#include "windowManager_temp.h"
+#include "video.h"
+#include "../core/cpu.h"
+
+
+int readControls(state *cpuState) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                return 1;
+            case SDL_KEYDOWN:
+                // check info at SDL documentation page: https://wiki.libsdl.org/SDL2/SDL_Event
+                switch (event.key.keysym.scancode) {
+                    case SDL_SCANCODE_C:
+                        // coin (0 when active?)
+                        // port testing to advance splash screen - not currently working
+                        setPort(cpuState, 1, 1, 0x01);
+                        setReg8(cpuState, A , 0x01);
+
+                        break;
+                    case SDL_SCANCODE_1:
+                        // p1 start button
+                        break;
+                    case SDL_SCANCODE_SPACE:
+                        // p1 shoot
+                        break;
+
+                    case SDL_SCANCODE_LEFT:
+                        // p1 move left
+                        break;
+
+                    case SDL_SCANCODE_RIGHT:
+                        // p1 move right
+                        break;
+                    case SDL_SCANCODE_ESCAPE:
+                        // TODO: keep for testing; this will close the window by tapping ESC key
+                        return 1;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    return 0;
+}
