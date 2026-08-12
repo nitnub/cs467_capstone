48a40f2f4fb586252fdfaea14c8cb63e028a70a4
liashl <liahardin@gmail.com>
2026-08-11 22:07:53 -0400

    debugger wired in with script for WSL

--- main.c
+++ main.c
@@ -68,15 +68,7 @@ int setupEmulator(struct instructionData *currentIns, Media_t *mediaBucket, Audi
 }
 
 
-int main(void) {
-
-    // verify valid game files exist
-    if (!gameFilesExist()){
-        printf("Error opening game files. Pleasa place your unzipped game_files folder in the project's root directory.\n");
-        return 1;
-    }
-
-
+int main(int argc, char* argv[]) {
     ////////////////////////
     // Set Up Application //
     ////////////////////////
@@ -90,6 +82,26 @@ int main(void) {
     ins.s = &myCpu;
     setupEmulator(&ins, mediaBucket, &audio);
 
+    ///////////////////////
+    // Debug status      //
+    ///////////////////////
+    int debug;
+    struct instructionData *disassembler = NULL;
+    if (argc < 2) {
+        debug = 0;
+    }
+    else if (!strcmp(argv[1], "debug")) {
+        debug = 1;
+        
+        // set up secondary CPU for disassembly
+        disassembler = &dis;
+        dis.s = &disCpu;
+        setupMemory(&disCpu, 0x00);
+        setupCPU(&disCpu.currentOp);
+    }
+    else {
+        debug = 0;
+    }
 
     ////////////////////////
     // Main Emulator Loop //
@@ -106,7 +118,7 @@ int main(void) {
     SDL_SetWindowTitle(mediaBucket->window, WINDOW_TITLE_SPACE_INVADERS);
 
     // run game loop...
-    runIntel8080(&ins, mediaBucket, &audio);
+    runIntel8080(&ins, mediaBucket, &audio, debug, disassembler);
 
 
     ///////////////////////////
